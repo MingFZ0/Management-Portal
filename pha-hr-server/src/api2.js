05fba6fe28ed7f1df1f54e1d9fe1c5e644ba4152
Mingfeng Zhong <mingfengz1678@gmail.com>
2025-09-14 13:58:46 -0400

    stubbed out the api

---
+++ pha-hr-server/src/api2.js
@@ -0,0 +1,74 @@
+//#region Users
+/**
+ * Creates an user entry in the User collection
+ * @param {Json} data : Consists of fields for the User
+ *      {
+ *          first_name,
+ *          last_name,
+ *          contact: {email, address}}
+ */
+async function addUser(data) {
+}
+
+
+async function getAllUsers(params) {
+    
+}
+
+async function getUser(params) {
+    
+}
+
+async function updateUser(params) {
+    
+}
+
+async function deleteUser(params) {
+    
+}
+    //#endregion
+
+//#region Departments
+async function createDepartment(params) {
+    
+}
+
+async function getAllDepartment(params) {
+    
+}
+
+async function getDepartment(params) {
+    
+}
+
+async function updateDepartment(params) {
+    
+}
+
+async function deleteDepartment(params) {
+    
+}
+    //#endregion
+
+
+//#region Hires
+async function createHire() {
+
+}
+
+async function getAllHires(params) {
+    
+}
+
+async function getHire(params) {
+    
+}
+
+async function updateHire(params) {
+    
+}
+
+async function deleteHire(params) {
+    
+}
+    //#endregion
