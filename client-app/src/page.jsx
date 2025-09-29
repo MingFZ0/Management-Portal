5219e333af26a5784d2359ba45a9a9077e6a8ea0
Mingfeng Zhong <mingfengz1678@gmail.com>
2025-09-29 10:05:38 -0400

    Moved client-app from setup to new directory

---
+++ client-app/src/page.jsx
@@ -0,0 +1,12 @@
+import DBPage from './dbpage.jsx';
+import DBPage2 from "./dbpage2";
+import ButtonUsage from "./button_test";
+
+import BasicTable from "./mtable";
+
+export default function Home() {
+  return (
+    <BasicTable></BasicTable>
+
+  );
+}
