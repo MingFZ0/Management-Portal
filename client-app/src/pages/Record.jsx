7fd4d11895386cc8dcf7f442b21d7428d10b6880
Mingfeng Zhong <mingfengz1678@gmail.com>
2025-10-01 00:08:10 -0400

    All collections now can be displayed

--- client-app/src/pages/Record.jsx
+++ client-app/src/pages/Record.jsx
@@ -1,43 +1,95 @@
-import { Box, Button, CardContent, CardHeader } from "@mui/material";
+import { Box, Button, CardContent, CardHeader, Tab, Tabs } from "@mui/material";
 import { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import RecordPanel from "./RecordPanel";
+import { SearchRecord } from "./SearchRecord";
 
 export function Record() {
 
-    const [collectionData, setCollectionData] = useState([]);
+    
 
-    const columns = [
+    let currentColumns = [];
+    const userColumns = [
         {field:"id", headName: "ID", width: 100},
-        {field:"first_name", headName: "First Name", width: 70},
-        {field:"last_name", headName: "Last Name", width: 70},
-        {field: "email", headName: "Email", width: 70},
+        {field:"first_name", headName: "First Name"},
+        {field:"last_name", headName: "Last Name"},
+        {field: "email", headName: "Email"},
         {field: "address", headName: "Address", width: 70},
-        {field:"title", headName: "Title", width: 70},
-        {field:"department", headName: "Department", width: 70},
+    ]
+
+    const departmentColumns = [
+        {field:"id", headName: "ID", width: 100},
+        {field:"name", headName: "Department", width: 180},
+    ]
+
+    const hireColumns = [
+        {field:"id", headName: "ID", width: 100},
+        {field:"first_name", headName: "First Name"},
+        {field:"last_name", headName: "Last Name"},
+        {field: "email", headName: "Email"},
+        {field: "address", headName: "Address", width: 70},
+        {field:"title", headName: "Title", width: 200},
+        {field:"department", headName: "Department", width: 180},
         {field:"salary", headName: "Salary", type: 'number'}
     ]
 
+    const [collectionData, setCollectionData] = useState([]);
+    const [tabValue, setTabValue] = useState(0);
+    const [columnData, setColumnData] = useState(hireColumns);
+    const [rowData, setRowData] = useState();
+
     async function getData() {
 
-        let userData = null;
-        let departmentData = null;
+        let userData = [];
+        let departmentData = [];
         let hireData = [];
         try {
             let apiUrl = import.meta.env.VITE_API_URL;
             console.log(`Getting Data; API URL is ${apiUrl}`);
             
             let userResult = await fetch(`http://${apiUrl}/users`);
-            // console.log(userResult);
-            userData = await userResult.text();
 
             let departmentResult = await fetch(`http://${apiUrl}/departments`);
-            // console.log(departmentResult);
-            departmentData = await departmentResult.text();
 
             let hireResult = await fetch(`http://${apiUrl}/hires`);
             console.log("HJIRE: " + hireResult.status);
             // console.log(hireResult)
+
+            if (userResult.status == 200) {
+                let rawData = await userResult.text();
+                let data = JSON.parse(rawData);
+                console.log(data)
+                for (let i = 0; i < data.length; i++) {
+                    let email = null;
+                    let address = null;
+
+                    if (data[i].contact != null) {
+                        email = data[i]["contact"]["email"];
+                        address = data[i]["contact"]["address"];
+                    }
+
+                    let row = {
+                        id: data[i]["_id"],
+                        first_name: data[i]["first_name"],
+                        last_name: data[i]["last_name"],
+                        email: email,
+                        address: address,
+                    };
+                    userData.push(row);
+                }
+            }
+
+            if (departmentResult.status == 200) {
+                let rawData = await departmentResult.text();
+                let data = JSON.parse(rawData);
+                for (let i = 0; i < data.length; i++) {
+                    let row = {
+                        id: data[i]["_id"],
+                        name: data[i]["name"],
+                    };
+                    departmentData.push(row);
+                }
+            }
 
             if (hireResult.status == 200) {
                 let rawHireData = await hireResult.text();
@@ -63,23 +115,18 @@ export function Record() {
                         title: tempHireData[i]["title"],
                         salary: tempHireData[i]["salary"]
                     };
-
-                    // console.log(row);
                     hireData.push(row);
                 }
             }
-
-           
-
             try {
+                console.log(hireData);
                 setCollectionData({users: userData, departments: departmentData, hires: hireData});
+                setRowData(hireData);
+                
                 
             } catch (err) {
                 console.log("JSON parse:" + err);
             }
-            // console.log(userData);
-            // console.log(departmentData);
-            // console.log(hireData);
 
         } catch (err) {
             console.log("Fetch failed: " + err);
@@ -88,20 +135,45 @@ export function Record() {
         return;
     }
 
-    useEffect(()=> {setTimeout(getData, 0)}, [])
-    console.log(collectionData.hires);
+    async function handleTabChange(event, value) {
+        setTabValue(value);
+        if (value == 0) {
+            console.log(collectionData.hires);
+            setRowData(collectionData.hires);
+            setColumnData(hireColumns);
+        }
+        else if (value == 1) {
+            console.log(collectionData.departments);
+            setRowData(collectionData.departments);
+            setColumnData(departmentColumns);
+        }
+        else {
+            console.log(collectionData.users);
+            setRowData(collectionData.users);
+            setColumnData(userColumns);
+        }
+    }
 
+    useEffect(() => {setTimeout(getData, 0)}, []);
     
 
+
     return (
-        <Box>
-            <Button className="FilterButton" variant="contained">Filter</Button>
+        <Box>    
+            <Tabs value={tabValue} onChange={handleTabChange} className="tabs">
+                <Tab label="Hires" value={0}></Tab>
+                <Tab label="Departments" value={1}></Tab>
+                <Tab label="Users" value={2}></Tab>
+            </Tabs>
+            {/* <SearchRecord></SearchRecord> */}
             <DataGrid
-                columns={columns}
-                rows={collectionData.hires}
+                columns={columnData}
+                rows={rowData}
                 checkboxSelection
+                initialState={{pagination: { page: 0, pageSize: 5, rowsPerPage: 15 }}}
+                // pageSizeOptions={[5, 10]}
                 sx={{ border: 0 }}
-                />
+            />
         </Box>
     )
 }
